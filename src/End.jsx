f668e3e3f5b536519549468d05605ec8522655e0
Karma Riuk <riukkarma@gmail.com>
2022-12-23 15:01:51 +0100

    yes

--- src/End.jsx
+++ src/End.jsx
@@ -4,16 +4,21 @@ import react_context from "./context";
 
 function toCSV(context) {
     const { name, age, prog } = context.userInfo;
+    let header = ["name", "age", "prog"];
     let body = [name, age, prog];
     let results = [...context.results];
     results.sort((a, b) => a._id - b._id);
     results.forEach(e => {
+        header.push(`success ${e._id}`);
+        header.push(`phrase length ${e._id}`);
+        header.push(`time ${e._id}`);
+
         body.push(e.success);
         body.push(e.phrase_length);
         body.push(e.time);
     });
 
-    return [body];
+    return [header, body];
 }
 
 export function End() {
